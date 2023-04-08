7c360828a0c7ee662ce96574507a49aeecaaae86
Ellayve <124088717+Ellayve@users.noreply.github.com>
2023-04-08 15:28:42 -0400

    Vercel

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express")
+const express = require("express");
 //const { graphqlHTTP } = require('express-graphql');
-const typeDefs = require("./graphql/schema")
-const resolvers = require("./graphql/resolvers")
-const mongoose = require("mongoose")
-const cors = require("cors")
+const typeDefs = require("./graphql/schema");
+const resolvers = require("./graphql/resolvers");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const { ApolloServer } = require('apollo-server-express');
+const { createServer } = require('http');
+const { Vercel } = require('@vercel/node');
+
+
 
 //const app = express()
 
@@ -30,16 +34,24 @@ mongoose.connect('mongodb+srv://elhamvei:Abc_1234@cluster0.quqgho6.mongodb.net/c
 //app.listen(3000, () => { console.log('Server is running...') });
 
 const app = express();
-
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-(async () => {
+// (async () => {
+//   await server.start();
+//   server.applyMiddleware({ app });
+
+//   app.listen({ port: 80 }, () =>
+//     console.log(`Server ready at http://localhost:80/graphql`)
+//   );
+// })();
+
+module.exports = Vercel(async (req, res) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  app.listen({ port: 80 }, () =>
-    console.log(`Server ready at http://localhost:80/graphql`)
-  );
-})();
+  const handler = createServer(app);
+  handler(req, res);
+});
